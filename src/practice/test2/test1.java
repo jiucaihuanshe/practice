4432521acf4639daca1bf813923ddc08fab287ea
jiucaihuanshe <948710286@qq.com>
2018-06-20 16:48:08 +0800

    test

--- src/practice/test2/test1.java
+++
@@ -1,5 +0,0 @@
-package practice.test2;
-
-public class test1 {
-
-}
---
+++ src/practice/testAll.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class testAll {
+	public static void main(String argv[]) {
+		int a = 12;
+		a *= 2 + 3;
+		System.out.println(a);
+	}
+	
+}
